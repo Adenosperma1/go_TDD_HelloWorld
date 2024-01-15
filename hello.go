05d27e79bb0e9c82f0b211c64900770e04ab97b0
Brendan Wilde <44901073+Adenosperma1@users.noreply.github.com>
2024-01-16 07:11:42 +1100

    Go_helloWorld_TDD

--- hello.go
+++ hello.go
@@ -2,10 +2,45 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const (
+	defaultName = "World"
+
+	spanish = "Spanish"
+	french = "French"
+	japanese = "Japanese"
+
+	helloEnglish = "Hello"
+	helloSpanish = "Hola"
+	helloFrench = "Bonjour"
+	helloJapanese = "Kon'nichiwa"
+	commaSpace = ", "
+)
+ 
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = defaultName
+	}
+return helloIn(language) + commaSpace + name
 }
 
+func helloIn(language string) (hello string){
+	switch language {
+	case french:
+		hello = helloFrench
+	case spanish:
+		hello = helloSpanish
+	case japanese:
+		hello = helloJapanese
+	default:
+		hello = helloEnglish
+	}
+
+	return hello
+}
+
+
+
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("World", "English"))
 }
--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,67 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("say hello with a person's name", func(t *testing.T) {
 
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+
+		if got != want {
+			printError(t, got, want)
+		}
+
+	})
+
+	t.Run("Don't supply a name default to hello world in English", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		if got != want {
+			printError(t, got, want)
+		}
+	})
+
+
+	t.Run("Don't supply a name default to hello world in Spanish", func(t *testing.T) {
+		got := Hello("", "Spanish")
+		want := "Hola, World"
+
+		if got != want {
+			printError(t, got, want)
+		}
+	})
+
+	t.Run("in Spanish", func(t *testing.T){
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		if got != want {
+			printError(t, got, want)
+		}
+	})
+
+	t.Run("in French", func(t *testing.T){
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+		if got != want {
+			printError(t, got, want)
+		}
+	})
+
+	t.Run("in Japanese", func(t *testing.T){
+		got := Hello("Elodie", "Japanese")
+		want := "Kon'nichiwa, Elodie"
+		if got != want {
+			printError(t, got, want)
+		}
+	})
+	
+}
+
+
+
+
+func printError(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
